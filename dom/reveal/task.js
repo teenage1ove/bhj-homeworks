9a816aed571e607b8ab93c4b2caff3e2924a6c62
teenage1ove <kirillstars7@gmail.com>
2024-10-17 12:12:06 +0300

    Add task

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,9 +1,11 @@
-const reveal = document.querySelector('.reveal')
+const reveal = document.querySelectorAll('.reveal')
 
 document.addEventListener('scroll', () => {
-	if (reveal.getClientRects()[0].top < window.innerHeight) {
-		reveal.classList.add('reveal_active')
-	}
+	reveal.forEach(reveal => {
+		if (reveal.getClientRects()[0].top < window.innerHeight) {
+			reveal.classList.add('reveal_active')
+		}
+	})
 
 	// if (reveal.getClientRects()[0].top > window.innerHeight) {
 	//     reveal.classList.remove('reveal_active');
